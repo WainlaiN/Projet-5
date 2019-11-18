f91b54b03f4ebeaf33595124d6bbff8d79424954
WainlaiN <nicodupriez@hotmail.com>
2019-11-18 14:48:36 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Ma page web</title>
+    </head>
+    <body>
+        <h1>Ma page web</h1>
+        <p>Aujourd'hui nous sommes le <?php phpinfo; ?>.</p>
+    </body>
+</html>
+
+<?php
+<e
+
+?>
+
