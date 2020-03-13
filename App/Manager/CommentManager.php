b876b8de6b5ec077a2ddf64c241c28bd76df92c0
Nicolas <nicodupriez@hotmail.com>
2020-03-13 12:45:38 +0100

    Added Name and forename in contact form email

--- App/Manager/CommentManager.php
+++ App/Manager/CommentManager.php
@@ -19,7 +19,7 @@ class CommentManager extends Database
      */
     public function getComments($postId)
     {
-        $req = 'SELECT comment_id, author_id, comment, is_valid, post_id, users.username, DATE_FORMAT(comment_date, \'%d/%m/%Y\') AS comment_date FROM comments INNER JOIN users on comments.author_id=users.user_id WHERE post_id = :postId ORDER BY comment_date DESC';
+        $req = 'SELECT comment_id, author_id, comment, is_valid, post_id, users.username, DATE_FORMAT(comment_date, \'%d/%m/%Y\') AS commentdate FROM comments INNER JOIN users on comments.author_id=users.user_id WHERE post_id = :postId ORDER BY commentdate DESC';
         $parameters = [':postId' => $postId];
         $result = $this->sql($req, $parameters);
         $custom_array = [];
