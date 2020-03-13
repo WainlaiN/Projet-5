efca330446c5c13475d300806ee529af824727c5
Nicolas <nicodupriez@hotmail.com>
2020-03-13 11:41:01 +0100

    Added Name and forename in contact form email

--- App/Manager/FormManager.php
+++ App/Manager/FormManager.php
@@ -26,7 +26,7 @@ class FormManager
 
         $mailer = new \Swift_Mailer($transport);
 
-        $message = (new \Swift_Message('Message du Blog'))
+        $message = (new \Swift_Message('Message du Blog de ' . $name . ' ' .$forename))
             ->setFrom($email)
             ->setTo($data['email'])
             ->setBody($message);
