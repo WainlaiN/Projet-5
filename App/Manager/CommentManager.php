51ff16afb1f6e4f6633cfabaf6df815c47c8a4a1
Nicolas <nicodupriez@hotmail.com>
2020-03-13 09:00:12 +0100

    Fixing CommentManager using INNER JOIN

--- App/Manager/CommentManager.php
+++ App/Manager/CommentManager.php
@@ -13,7 +13,6 @@ class CommentManager extends Database
 {
 
     protected $model = Comment::class;
-    protected $table_name = 'comments';
 
     /**
      * Return Comments from a post
@@ -23,7 +22,7 @@ class CommentManager extends Database
      */
     public function getComments($postId)
     {
-        $req = 'SELECT comments.id, author_id, comment, is_valid, post_id, users.username, DATE_FORMAT(comment_date, \'%d/%m/%Y\') AS comment_date FROM ' . $this->table_name . ' INNER JOIN users on comments.author_id=users.userid WHERE post_id = :postId ORDER BY comment_date DESC';
+        $req = 'SELECT comments.id, author_id, comment, is_valid, post_id, users.username, DATE_FORMAT(comment_date, \'%d/%m/%Y\') AS comment_date FROM comments INNER JOIN users on comments.author_id=users.userid WHERE post_id = :postId ORDER BY comment_date DESC';
         $parameters = [':postId' => $postId];
         $result = $this->sql($req, $parameters);
 
@@ -45,7 +44,7 @@ class CommentManager extends Database
      */
     public function getComment($commentId)
     {
-        $req = 'SELECT comments.id, author_id, comment, is_valid, post_id, users.username, DATE_FORMAT(comment_date, \'%d/%m/%Y\') AS comment_date FROM ' . $this->table_name . ' INNER JOIN users on comments.author_id=users.userid WHERE comments.id = :id';
+        $req = 'SELECT comments.id, author_id, comment, is_valid, post_id, users.username, DATE_FORMAT(comment_date, \'%d/%m/%Y\') AS comment_date FROM comments INNER JOIN users on comments.author_id=users.userid WHERE comments.id = :id';
         $parameters = [':id' => $commentId];
         return $this->sql($req, $parameters);
     }
@@ -61,7 +60,7 @@ class CommentManager extends Database
      */
     public function addComment($postId, $authorId , $comment)
     {
-        $newComments = 'INSERT INTO ' . $this->table_name . '(post_id, author_id, comment, comment_date) VALUES (:postId,:authorId,:comment, DATE(NOW()))';
+        $newComments = 'INSERT INTO comments(post_id, author_id, comment, comment_date) VALUES (:postId,:authorId,:comment, DATE(NOW()))';
         $parameters = [':postId' => $postId, ':authorId' => $authorId, ':comment' => $comment];
 
         return $this->sql($newComments, $parameters);
@@ -76,7 +75,7 @@ class CommentManager extends Database
      */
     public function deleteComment($commentId)
     {
-        $comment = 'DELETE FROM ' . $this->table_name . ' WHERE id= :id';
+        $comment = 'DELETE FROM comments WHERE id= :id';
         $parameters = [':id' => $commentId];
         $result = $this->sql($comment, $parameters);
         return $result;
@@ -90,7 +89,7 @@ class CommentManager extends Database
      */
     public function getValidComments($postId)
     {
-        $validComments = 'SELECT comments.id, author_id, comment, is_valid, post_id, users.username, DATE_FORMAT(comment_date, \'%d/%m/%Y\') AS comment_date FROM ' . $this->table_name . ' INNER JOIN users on comments.author_id=users.userid WHERE is_valid = :valid AND post_id = :postid';
+        $validComments = 'SELECT comments.id, author_id, comment, is_valid, post_id, users.username, DATE_FORMAT(comment_date, \'%d/%m/%Y\') AS comment_date FROM comments INNER JOIN users on comments.author_id=users.userid WHERE is_valid = :valid AND post_id = :postid';
         $parameters = [':valid' => 1, ':postid' => $postId];
         $result = $this->sql($validComments, $parameters);
         if ($result->rowCount() > 1) {
@@ -110,7 +109,7 @@ class CommentManager extends Database
      */
     public function getInvalidComments()
     {
-        $invalidComments = 'SELECT * FROM ' . $this->table_name . ' WHERE is_valid = :valid';
+        $invalidComments = 'SELECT * FROM comments WHERE is_valid = :valid';
         $parameters = [':valid' => 0];
         $result = $this->sql($invalidComments, $parameters);
         if ($result->rowCount() > 1) {
@@ -131,7 +130,7 @@ class CommentManager extends Database
      */
     public function validateComment($commentId)
     {
-        $validate = 'UPDATE ' . $this->table_name . ' SET is_valid = :valid WHERE id = :id';
+        $validate = 'UPDATE comments SET is_valid = :valid WHERE id = :id';
         $parameters = [':id' => $commentId, ':valid' => 1];
         $result = $this->sql($validate, $parameters);
         return $result;
