9fb6477e4385d6db2573d2d0d4e3b790a08d8d34
Nicolas <nicodupriez@hotmail.com>
2020-06-11 11:21:03 +0200

    test

--- index.php
+++ index.php
@@ -9,32 +9,32 @@ $router = new router();
 $router->setBasePath('/Projet5');
 
     //Front management
-    $router->map('GET', '/', 'FrontController#home');
-    $router->map('GET', '/posts', 'FrontController#listPosts');
-    $router->map('GET', '/post/[i:id]', 'FrontController#post');
-    $router->map('GET', '/register', 'FrontController#registerView');
-    $router->map('POST', '/register', 'FrontController#register');
-    $router->map('POST', '/comment/add', 'FrontController#addComment');
-    $router->map('POST', '/', 'FrontController#contactForm');
-    $router->map('GET', '/CV', 'FrontController#cvNico');
-    $router->map('GET', '/CGV', 'FrontController#getCGV');
+    $router->map('GET', 'Projet5/', 'FrontController#home');
+    $router->map('GET', 'Projet5/posts', 'FrontController#listPosts');
+    $router->map('GET', 'Projet5/post/[i:id]', 'FrontController#post');
+    $router->map('GET', 'Projet5/register', 'FrontController#registerView');
+    $router->map('POST', 'Projet5/register', 'FrontController#register');
+    $router->map('POST', 'Projet5/comment/add', 'FrontController#addComment');
+    $router->map('POST', 'Projet5/', 'FrontController#contactForm');
+    $router->map('GET', 'Projet5/CV', 'FrontController#cvNico');
+    $router->map('GET', 'Projet5/CGV', 'FrontController#getCGV');
 
     //Admin Management
 
-    $router->map('GET', '/admin', 'AdminController#listPosts');
-    $router->map('GET', '/admin/post/[i:id]', 'AdminController#updatePostView');
-    $router->map('POST', '/admin/post/[i:id]', 'AdminController#UpdatePost');
-    $router->map('POST', '/admin/post/delete/[i:id]', 'AdminController#deletePost');
-    $router->map('GET', '/admin/add', 'AdminController#addPostView');
-    $router->map('POST', '/admin/addpost', 'AdminController#addPost');
-    $router->map('GET', '/admin/comments/[i:id]', 'AdminController#listComments');
-    $router->map('POST', '/admin/comment/delete/[i:id]', 'AdminController#deleteComment');
-    $router->map('POST', '/admin/comment/validate/[i:id]', 'AdminController#validateComment');
+    $router->map('GET', 'Projet5/admin', 'AdminController#listPosts');
+    $router->map('GET', 'Projet5/admin/post/[i:id]', 'AdminController#updatePostView');
+    $router->map('POST', 'Projet5/admin/post/[i:id]', 'AdminController#UpdatePost');
+    $router->map('POST', 'Projet5/admin/post/delete/[i:id]', 'AdminController#deletePost');
+    $router->map('GET', 'Projet5/admin/add', 'AdminController#addPostView');
+    $router->map('POST', 'Projet5/admin/addpost', 'AdminController#addPost');
+    $router->map('GET', 'Projet5/admin/comments/[i:id]', 'AdminController#listComments');
+    $router->map('POST', 'Projet5/admin/comment/delete/[i:id]', 'AdminController#deleteComment');
+    $router->map('POST', 'Projet5/admin/comment/validate/[i:id]', 'AdminController#validateComment');
 
     //login Management
-    $router->map('GET', '/login', 'FrontController#login');
-    $router->map('POST', '/connect', 'FrontController#connect');
-    $router->map('GET', '/logout', 'FrontController#deconnect');
+    $router->map('GET', 'Projet5/login', 'FrontController#login');
+    $router->map('POST', 'Projet5/connect', 'FrontController#connect');
+    $router->map('GET', 'Projet5/logout', 'FrontController#deconnect');
 
     $match = $router->match();
 
